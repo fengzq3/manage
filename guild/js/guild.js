8248cbcfe64a0785b81e19f63f25501f20beb342
fengpc <zhiqiang.feng@qq.com>
2016-05-06 18:11:54 +0800

    guild：更换动画方式

--- guild/js/guild.js
+++ guild/js/guild.js
@@ -4,30 +4,126 @@
 
 $(function () {
 
-    //图片移动
+    //元素
     var guildBgP = $('#guildBgP'), guildPerson = $('.guild-person');
 
-    //初始载入
-    var durTime = (200 * $(document).width())/100;
+    //初始载入 小人走入画面
+    //根据移动距离计算时间
     guildPerson.addClass('guild-start').animate(
-        {left: '20%'},
+        {left: '500px'},
         {
-            duration: durTime,
+            duration: 6000,
             easing: 'linear',
-            complete: function () {
-                setp1();
-            }
+            complete: setp1
         }
     );
 
     //进入flow
-
     function setp1() {
-        guildPerson.removeClass('guild-start');
-        $('.guild-setp1').delay(1000).animate({bottom: '-20px'}, {
-            duration: 500, easing: 'easeOutBounce'
+        //停止走动
+        //guildPerson.removeClass('guild-start');
+        //step1
+        var step = [], i = 0;
+        step[0] = $('#setp1');
+        step[1] = $('#setp1-1');
+        step[2] = $('#setp1-2');
+        step[3] = $('#setp1-3');
+        step[4] = $('#setp2');
+        step[5] = $('#setp2-1');
+        step[6] = $('#setp2-2');
+        step[7] = $('#setp3');
+        step[8] = $('#setp3-1');
+        step[9] = $('#setp3-2');
+
+
+        //继续走动
+        var moveBgA = moveBg();
+        moveBgA.startMove();
+
+        guildBgP.on('guildMove', function (e, leftL) {
+            setActive();
+            //判断元素位置
+            function setActive() {
+                if (i < step.length) {
+
+                    if ((step[i].offset().left - ($(document).width() - 1200) / 2) < 600) {
+
+                        //开始处理动作
+                        if (i == 0 || i == 4 || i == 7) {
+                            bounceSign(step[i]);
+                        } else {
+                            step[i].addClass('zoomIn').delay(1000);
+                            //停止运动一秒
+                            console.log('停止');
+                            guildPerson.removeClass('guild-start');
+                            moveBgA.stopMove();
+                            //一秒后开始
+                            setTimeout(function () {
+                                moveBgA.startMove(true);
+                                guildPerson.addClass('guild-start');
+                            }, 1000);
+
+                        }
+
+                        //循环判断
+                        i++;
+                        setActive();
+                    }
+                } else {
+                    return false;
+                }
+
+            }
+
         });
+
+
     }
+
+    //弹出标牌
+    function bounceSign(obj) {
+        obj.animate(
+            {marginBottom: 0, opacity: 1},
+            {
+                duration: 500,
+                easing: 'easeOutBounce'
+            }
+        );
+    }
+
+
+    //幕布移动方法
+    function moveBg() {
+        var leftL =0, timer, flag = true;
+
+        function startMove(flg) {
+            if (!!flg) {
+                flag = true;
+            }
+
+            leftL--;
+            guildBgP.css('left', leftL);
+            guildBgP.trigger('guildMove', [leftL]);
+            if (leftL > -9617 && flag) {
+                timer = setTimeout(startMove, 10);
+            } else {
+                clearTimeout(timer);
+                guildBgP.trigger('guildStop', [leftL]);
+            }
+        }
+
+        function stopMove() {
+            clearTimeout(timer);
+            flag = false;
+        }
+
+        return {
+            startMove: startMove,
+            stopMove: stopMove
+        }
+
+    }
+
 
 });
 
